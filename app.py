7cfb242b032178127f700fd0639046479ef9675e
NileshMete <107758842+NileshMete@users.noreply.github.com>
2026-01-11 09:01:52 +0530

    Add main function to print message and create output file

---
+++ app.py
@@ -0,0 +1,8 @@
+def main():
+    print("Hello, GCP! This app was deployed via Jenkins.")
+    # Simulate creating an artifact (output file)
+    with open("build_output.txt", "w") as f:
+        f.write("Build Successful: Deployed from Jenkins")
+
+if __name__ == "__main__":
+    main()
