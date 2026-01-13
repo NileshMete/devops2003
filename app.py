1691993ff585181fed5ce9eafc14c99e2ed1cfb1
NileshMete <107758842+NileshMete@users.noreply.github.com>
2026-01-13 11:30:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ def main():
     print("Hello, GCP! This app was deployed via Jenkins.")
     # Simulate creating an artifact (output file)
     with open("build_output.txt", "w") as f:
-        f.write("Build Successful: Deployed from jenking aautomate 
-        ")
+        f.write("Build Successful: Deployed from jenking aautomate")
 
 if __name__ == "__main__":
     main()
