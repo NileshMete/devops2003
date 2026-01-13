11b2d424e728b9aaeba3d99099af44805e01959f
NileshMete <107758842+NileshMete@users.noreply.github.com>
2026-01-13 11:19:42 +0530

    Fix typo in build output message

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ def main():
     print("Hello, GCP! This app was deployed via Jenkins.")
     # Simulate creating an artifact (output file)
     with open("build_output.txt", "w") as f:
-        f.write("Build Successful: Deployed from github ci ")
+        f.write("Build Successful: Deployed from jenking aautomate 
+        ")
 
 if __name__ == "__main__":
     main()
