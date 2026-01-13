5895ee671cba24535945ff703ad6300d4c8ec8ab
NileshMete <107758842+NileshMete@users.noreply.github.com>
2026-01-13 10:59:07 +0530

    Update deployment message to reflect GitHub CI

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def main():
     print("Hello, GCP! This app was deployed via Jenkins.")
     # Simulate creating an artifact (output file)
     with open("build_output.txt", "w") as f:
-        f.write("Build Successful: Deployed from Jenkins")
+        f.write("Build Successful: Deployed from github ci ")
 
 if __name__ == "__main__":
     main()
